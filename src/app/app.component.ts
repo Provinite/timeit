bb7abfc79ee68ec9f747e18c7b119813ec20eb9a
Collin Driscoll <collin.driscoll@ndus.edu>
2017-07-07 14:24:24 -0500

    Initializes with 1 timer. Fixes #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { TimerComponent } from './timer.component';
 export class AppComponent implements OnInit {
   title: string = 'App';
   runtime: number = 0;
-  timers = [0,1,2,3,4,5,6,7,8];
+  timers = [0];
   @ViewChildren(TimerComponent) timerComponents: QueryList<TimerComponent>;
   
   ngOnInit(): void {
